5dc3d38c3569463e59cf1893387319689428e115
swaruparam <sramakr6@ncsu.edu>
2015-08-23 21:24:24 +0000

    Added README

---
+++ hw/code/1/README.md
@@ -0,0 +1,16 @@
+Screen Snap of the top level of my Repo):
+
+
+Output of the commands:
+
+python --version:
+
+pip help:
+
+easy_install -h:
+
+
+Screen shot of what it looks like to write code in my preferred Python IDE:
+
+
+Screenshot of output of okok.py test:
---
+++ hw/code/1/okok.py
@@ -0,0 +1,30 @@
+import time
+from ok import *
+  
+print time.strftime("%H:%M:%S\n")
+
+@ok
+def _ok1():
+    assert 1==1
+ 
+@ok
+def _ok2():
+    assert 2==1
+
+@ok
+def _ok3():
+    assert 3==3 
+
+@ok
+def _ok4():
+    assert 4==3
+
+@ok
+def _ok5():
+    assert 5==5
+    
+@ok
+def _ok4():
+    assert unittest.tries==6
+    assert unittest.fails==2
+    print unittest.score()
