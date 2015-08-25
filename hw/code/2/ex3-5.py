e3a87e3a6abc3f761910d0d960e07629f876cf43
swaruparam <sramakr6@ncsu.edu>
2015-08-25 23:34:41 +0000

    Exercise 3

---
+++ hw/code/2/ex3-5.py
@@ -0,0 +1,18 @@
+def grid (row,col):
+    row = 5*(row-1) +1
+    col = 5*(col-1) +1
+    for i in range(0,row):
+        for j in range(0,col):
+            if i%5 == 0 and j%5 == 0:
+                print "+",
+            elif i%5 == 0:
+                print "-",
+            elif j%5 == 0:
+                print "/",
+            else:
+                print " ",
+        print
+        
+grid(3,3)
+    
+      
---
+++ hw/code/2/think3.py
@@ -0,0 +1,15 @@
+def do_twice(f,value):
+    f(value)
+    f(value)
+    
+def print_twice(value):
+    print value
+    
+def do_four(f,value):
+    do_twice(f,value)
+    do_twice(f,value)
+
+do_twice(print_twice,'spam')
+
+do_four(print_twice,'spam')
+
