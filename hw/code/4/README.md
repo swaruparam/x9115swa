3b5176c994a4c448ceb2df814dafca1a25ca7d4c
swaruparam <sramakr6@ncsu.edu>
2015-10-01 23:50:43 +0000

    Completed simulated annealing

---
+++ hw/code/4/README.md
@@ -0,0 +1,9 @@
+##**Code 4: coding homework**
+
+####**Simulated Annealing:**
+
+Output of /code/4/simulatedAnnealing.py:
+
+![simAnneal_1](simAnneal_1.png)
+
+![simAnneal_2](simAnneal_2.png)
--- hw/code/4/simulatedAnnealing.py
+++ hw/code/4/simulatedAnnealing.py
@@ -1,101 +1,100 @@
-#from __future__ import print_function
 import random
 import math
+import timeit
 
-#def say(x): print(x, end="")
-
-#upperBound = 100000
-#lowerBound = -upperBound
-
-class simAnneal(object):
+class simAnneal():
     
-    #upperBound = 0;
-    #lowerBound = 0;
-    #energyMin = energyMax = 0
-    
+    #initializing min and max energies as 0 and 1 initially
     def __init__ (self):
-        self.upperBound = 1000
-        self.lowerBound = -self.upperBound
-        self.energyMin = self.energyMax = 0
-        
+        self.energyMin = 0
+        self.energyMax = 1
+    
+    #calculation of f1    
     def schaffer1(self,x):
         return x**2
     
+    #calculation of f2
     def schaffer2(self,x):
         return (x-2)**2
-        
+    
+    #computing min and max energies with a baseline run of 1000    
     def energyMinMax(self):
-        x1 = random.randrange(self.lowerBound, self.upperBound)
-        self.energyMin = self.schaffer1(x1) + self.schaffer2(x1)
-        self.energyMax = self.schaffer1(x1) + self.schaffer2(x1)
-        for ite in range(100000):
-            x2 = random.randrange(self.lowerBound, self.upperBound)
-            newEnergy = self.schaffer1(x2) + self.schaffer2(x2)
+        for it in range(1000):
+            x = random.random()
+            newEnergy = self.schaffer1(x) + self.schaffer2(x)
             if newEnergy < self.energyMin:
                 self.energyMin = newEnergy
             if newEnergy > self.energyMax:
                 self.energyMax = newEnergy
     
+    #adding f1 and f2 and return normalized energy
     def normEnergy(self,x):
         en = self.schaffer1(x) + self.schaffer2(x)
-        return en 
-        #print en, self.energyMin, self.energyMax
-        #print (en-self.energyMin) / (self.energyMax-self.energyMin)
-        #return (en-self.energyMin) / (self.energyMax-self.energyMin)
-        
+        return (en-self.energyMin) / (self.energyMax-self.energyMin)
+    
+    #to denormalize the energy    
+    def denormalize(self,x):
+        return (self.energyMax-self.energyMin)*x + self.energyMin
+    
+    #probability function    
     def prob(self, currEnergy, neighborEnergy, t):
-        p = float(currEnergy-neighborEnergy)/float(t)
-        ret = math.exp(p)
+        p = (currEnergy-neighborEnergy)/t
+        ret = math.e**p
         return ret
-        
+    
+    #simulated annealing starts    
     def simulatedAnnealing(self):
-        print self.energyMin
-        print self.energyMax
-        kmax = 1000
-        #emin = 0.00000000000001
-        s = random.randrange(self.lowerBound, self.upperBound)
-        #print s
-        sb = s
-        e = self.normEnergy(s)
-        #print e
-        eb = e
-        k = 1
+        print "Schaffer\n"
+        kmax = 1000.0
+        emax = 0        
+        sb = s = random.random()
+        eb = e = self.normEnergy(s)
+        k = 1.0
+        out = [ ]
         
-        while k <= kmax:# and e > emin:
+        while k < kmax and e > emax:
             
-            sn = random.randrange(self.lowerBound, self.upperBound)
+            sn = random.random()
             en = self.normEnergy(sn)
-            #print en,
+ 
             if en < eb:
                 sb = sn
                 eb = en
-                print "!",
+                out.append("!")
             
             elif en < e:
                 s = sn
                 e = en
-                print "+",
+                out.append("+")
             
-            elif self.prob(e,en, float(k)/float(kmax)) < random.random():
+            elif self.prob(e,en, (k/kmax)*5) < random.random():
                 s = sn
                 e = en
-                print "?",
+                out.append("?")
             
             else:
-                print ".",
-            
-            k = k+1 
+                out.append(".")
             
             if k%25 == 0:
-               print "\n"
-                # print "\n"
-        print "best state ", sb
-        print "best energy ", eb
+               print int(k), " - ",
+               print "".join(out)
+               out = [ ]
+            
+            k = k+1 
                 
+        print "\nKmax = ", kmax
+        print "Baseline = 1000", 
+        print "Cooling = 5"
+        print "Best state = ", sb
+        print "Best energy = %02f" % self.denormalize(eb)
+                
+start = timeit.default_timer()
 
 sa = simAnneal()
 sa.energyMinMax()
-#print(sa.energyMin)
-#print(sa.energyMax)
 sa.simulatedAnnealing()
 
+stop = timeit.default_timer()
+time = stop - start 
+
+print "Runtime = ", time
